e1f92abf9be515340c813f8541e087a425610415
cyjadela <adelacho@naver.com>
2023-06-20 16:25:29 +0900

    Feat: NetworkModule 추가

---
+++ coniverse_dangjang/data/src/main/java/com/dangjang/android/data/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.dangjang.android.data.di
+
+import com.dangjang.android.data.datasource.IntroDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = ""
+    @Provides
+    @Singleton
+    fun provideRequestHttpLoggingInterceptor() : HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideIntroDataSource(retrofit: Retrofit): IntroDataSource {
+        return retrofit.create(IntroDataSource::class.java)
+    }
+}
