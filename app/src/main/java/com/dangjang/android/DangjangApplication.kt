deb9f0761e670cda1db1f9da48af3a82b4640bb1
cyjadela <adelacho@naver.com>
2023-06-29 22:05:48 +0900

    Feat: 카카오 sdk 추가, 로그인 구현

--- app/src/main/java/com/dangjang/android/DangjangApplication.kt
+++ app/src/main/java/com/dangjang/android/DangjangApplication.kt
@@ -1,8 +1,15 @@
 package com.dangjang.android
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.v2.user.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class DangjangApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, "APP_KEY")
+    }
 }
--- presentation/src/main/java/com/dangjang/android/presentation/LoginActivity.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.dangjang.android.presentation
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import com.dangjang.android.presentation.databinding.ActivityLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +20,36 @@ class LoginActivity: FragmentActivity() {
         setContentView(R.layout.activity_login)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.btnKakaoLogin.setOnClickListener {
+            kakaoLogin()
+        }
+    }
+    private fun kakaoLogin() {
+        val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e("카카오/로그인 실패", error.toString())
+            } else if (token != null) {
+                Log.e("카카오/로그인 성공",token.accessToken)
+            }
+        }
 
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                if (error != null) {
+                    Log.e("카카오/로그인 실패", error.toString())
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+                    else {
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback)
+                    }
+                }
+                else if (token != null) {
+                    Log.e("카카오/로그인 성공",token.accessToken)
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback)
+        }
     }
 }
