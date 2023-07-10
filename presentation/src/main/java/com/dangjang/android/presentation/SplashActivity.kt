4eb1fb457dca22e4da69c4dc9df4a403c4f88ec3
cyjadela <adelacho@naver.com>
2023-07-10 11:44:49 +0900

    Feat: Splash 화면 구현

---
+++ presentation/src/main/java/com/dangjang/android/presentation/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.dangjang.android.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+}
