45313ae48eb5d431ce9588abb9ea0bbe82242888
cyjadela <adelacho@naver.com>
2023-07-15 17:52:25 +0900

    Chore: AndroidViewModel로 변경

--- presentation/src/main/java/com/dangjang/android/presentation/IntroViewModel.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/IntroViewModel.kt
@@ -1,21 +1,22 @@
 package com.dangjang.android.presentation
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.dangjang.android.domain.model.IntroVO
 import com.dangjang.android.domain.usecase.GetIntroUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class IntroViewModel @Inject constructor(
-    private val getIntroUseCase: GetIntroUseCase
-) : ViewModel() {
+    private val getIntroUseCase: GetIntroUseCase,
+    application: Application
+) : AndroidViewModel(application) {
 
     private val _introDataFlow = MutableStateFlow(IntroVO())
     val introDataFlow = _introDataFlow.asStateFlow()
