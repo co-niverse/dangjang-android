af2378342c1cd56c565fe3ad926119b0fe8b00e1
cyjadela <adelacho@naver.com>
2023-07-18 13:53:07 +0900

    Chore: handleErrors() 추가, Any type 삭제

--- presentation/src/main/java/com/dangjang/android/presentation/IntroViewModel.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/IntroViewModel.kt
@@ -7,8 +7,12 @@ import androidx.lifecycle.viewModelScope
 import com.dangjang.android.domain.model.IntroVO
 import com.dangjang.android.domain.usecase.GetIntroUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,13 +27,16 @@ class IntroViewModel @Inject constructor(
 
     fun getIntroData() {
         viewModelScope.launch {
-            getIntroUseCase.getIntro().collect{
-                if (it is Exception)
-                    Toast.makeText(getApplication<Application>().applicationContext,it.message,Toast.LENGTH_SHORT).show()
-                if (it is IntroVO)
+            getIntroUseCase.getIntro()
+                .onEach {
                     _introDataFlow.emit(it)
-            }
+                }
+                .handleErrors()
+                .collect()
         }
     }
+
+    private fun <T> Flow<T>.handleErrors(): Flow<T> =
+        catch { e -> Toast.makeText(getApplication<Application>().applicationContext,e.message,Toast.LENGTH_SHORT).show() }
 
 }
