e844cb9299f231d327065e46c6f29bc128cf8526
cyjadela <adelacho@naver.com>
2023-08-04 19:43:25 +0900

    Feat: 닉네임 중복 검사 (SignupDataSource)

---
+++ data/src/main/java/com/dangjang/android/data/datasource/SignupDataSource.kt
@@ -0,0 +1,15 @@
+package com.dangjang.android.data.datasource
+
+import com.dangjang.android.data.model.dto.DuplicateNicknameDto
+import com.dangjang.android.data.model.request.DuplicateNicknameRequest
+import com.dangjang.android.data.model.response.BaseResponse
+import javax.inject.Inject
+
+class SignupDataSource @Inject constructor(
+    private val signupApiService: SignupApiService
+) : BaseNetworkDataSource() {
+
+    suspend fun getDuplicateNickname(nickname: String): BaseResponse<DuplicateNicknameDto> {
+        return checkResponse(signupApiService.getDuplicateNickname(DuplicateNicknameRequest(nickname)))
+    }
+}
