0e01ebb7c98183236a9e97cb41bd76e65b5cb426
cyjadela <adelacho@naver.com>
2023-08-06 16:25:53 +0900

    Feat: Signup API - viewmodel 구현

--- presentation/src/main/java/com/dangjang/android/presentation/SignupViewModel.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/SignupViewModel.kt
@@ -1,10 +1,13 @@
 package com.dangjang.android.presentation
 
 import android.app.Application
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.dangjang.android.domain.model.DuplicateNicknameVO
+import com.dangjang.android.domain.model.SignupVO
+import com.dangjang.android.domain.requestVO.SignupRequestVO
 import com.dangjang.android.domain.usecase.SignupUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -13,7 +16,9 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,6 +30,12 @@ class SignupViewModel @Inject constructor(
     private val _duplicateNicknameFlow = MutableStateFlow(DuplicateNicknameVO())
     val duplicateNicknameFlow = _duplicateNicknameFlow.asStateFlow()
 
+    private val _signupFlow = MutableStateFlow(SignupVO())
+    val signupFlow = _signupFlow.asStateFlow()
+
+    private val _signupRequest = MutableStateFlow(SignupRequestVO())
+    val signupRequest = _signupRequest.asStateFlow()
+
     fun getDuplicateNickname(nickname: String) {
         viewModelScope.launch {
             getSignupUseCase.getDuplicateNickname(nickname)
@@ -35,6 +46,20 @@ class SignupViewModel @Inject constructor(
                 .collect()
         }
     }
+
+    fun signup(
+        data: SignupRequestVO
+    ) {
+        viewModelScope.launch {
+            getSignupUseCase.signup(data)
+                .onEach {
+                    _signupFlow.emit(it)
+                }
+                .handleErrors()
+                .collect()
+        }
+    }
+
 
     private fun <T> Flow<T>.handleErrors(): Flow<T> =
         catch { e -> Toast.makeText(getApplication<Application>().applicationContext,e.message,
