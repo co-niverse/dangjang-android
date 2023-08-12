8b629d61e8c0aa20226a0047b533378b69f947c1
cyjadela <adelacho@naver.com>
2023-08-12 16:34:52 +0900

    Feat: import Log, AdapterView

--- presentation/src/main/java/com/dangjang/android/presentation/signup/SignupGenderBirthFragment.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/signup/SignupGenderBirthFragment.kt
@@ -1,8 +1,10 @@
 package com.dangjang.android.presentation.signup
 
 import android.graphics.Color
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
