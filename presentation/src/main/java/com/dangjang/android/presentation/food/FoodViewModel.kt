4c2c171746a175754ed45aead5b66a04cc68beee
cyjadela <adelacho@naver.com>
2023-08-31 17:45:45 +0900

    Rename: challenge -> food

--- presentation/src/main/java/com/dangjang/android/presentation/food/FoodViewModel.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/food/FoodViewModel.kt
@@ -1,10 +1,10 @@
-package com.dangjang.android.presentation.challenge
+package com.dangjang.android.presentation.food
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ChallengeViewModel @Inject constructor(
+class FoodViewModel @Inject constructor(
 ) : ViewModel() {
 }
