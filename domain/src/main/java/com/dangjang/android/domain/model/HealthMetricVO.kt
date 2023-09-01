c267521d59dad2d481d198e5b7184b3a0b0c6267
cyjadela <adelacho@naver.com>
2023-09-01 21:18:09 +0900

    Feat: HealthMetricVO 추가

---
+++ domain/src/main/java/com/dangjang/android/domain/model/HealthMetricVO.kt
@@ -0,0 +1,15 @@
+package com.dangjang.android.domain.model
+
+data class HealthMetricVO(
+    val type: String = "",
+    val createdAt: String = "",
+    val unit: String = "",
+    val guide: GuideVO = GuideVO()
+)
+
+data class GuideVO(
+    val createdAt: String = "",
+    val type: String = "",
+    val alert: String = "",
+    val content: String = ""
+)
