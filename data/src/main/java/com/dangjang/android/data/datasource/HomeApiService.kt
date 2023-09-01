eca98a77cd12c5d920beee73d7cd762cf4a21ff2
cyjadela <adelacho@naver.com>
2023-09-01 21:17:27 +0900

    Feat: 건강지표 등록 API 구현 (data layer)

---
+++ data/src/main/java/com/dangjang/android/data/datasource/HomeApiService.kt
@@ -0,0 +1,17 @@
+package com.dangjang.android.data.datasource
+
+import com.dangjang.android.data.model.dto.HealthMetricDto
+import com.dangjang.android.domain.request.AddHealthMetricRequest
+import com.dangjang.android.data.model.response.BaseResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface HomeApiService {
+
+    //건강지표 등록 API
+    @POST("/api/health-metric")
+    suspend fun addHealthMetric(
+        @Body addHealthMetricRequest: AddHealthMetricRequest
+    ) : Response<BaseResponse<HealthMetricDto>>
+}
---
+++ data/src/main/java/com/dangjang/android/data/datasource/HomeDataSource.kt
@@ -0,0 +1,15 @@
+package com.dangjang.android.data.datasource
+
+import com.dangjang.android.data.model.dto.HealthMetricDto
+import com.dangjang.android.domain.request.AddHealthMetricRequest
+import com.dangjang.android.data.model.response.BaseResponse
+import javax.inject.Inject
+
+class HomeDataSource @Inject constructor(
+    private val homeApiService: HomeApiService
+): BaseNetworkDataSource() {
+
+    suspend fun addHealthMetric(addHealthMetricRequest: AddHealthMetricRequest): BaseResponse<HealthMetricDto> {
+        return checkResponse(homeApiService.addHealthMetric(addHealthMetricRequest))
+    }
+}
