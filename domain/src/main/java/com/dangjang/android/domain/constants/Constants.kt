978f39a28a734de8329a11f62895e9c32c15424f
cyjadela <adelacho@naver.com>
2023-09-08 15:02:49 +0900

    Feat: 헬스커넥트 관련 상수 추가

--- domain/src/main/java/com/dangjang/android/domain/constants/Constants.kt
+++ domain/src/main/java/com/dangjang/android/domain/constants/Constants.kt
@@ -1,8 +1,14 @@
 package com.dangjang.android.domain.constants
 
+// Shared Preference
 const val AUTO_LOGIN_SPF_KEY = "auto"
 const val AUTO_LOGIN_EDITOR_KEY = "isAuto"
 const val NAVER = "naver"
 const val KAKAO = "kakao"
 const val TOKEN_SPF_KEY = "token"
 const val ACCESS_TOKEN_KEY = "AccessToken"
+
+// Health Connect
+const val HEALTH_CONNECT_UNAVAILABLE = 1
+const val HEALTH_CONNECT_NOT_INSTALLED = 2
+const val HEALTH_CONNECT_INSTALLED = 3
