81e0236be87be90519afd74754a7de31ccec3ce0
cyjadela <adelacho@naver.com>
2023-09-11 15:45:00 +0900

    Feat: 혈당 가이드 Adapter 구현

---
+++ presentation/src/main/java/com/dangjang/android/presentation/home/GlucoseGuideAdapter.kt
@@ -0,0 +1,41 @@
+package com.dangjang.android.presentation.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dangjang.android.domain.model.GlucoseGuideVO
+import com.dangjang.android.presentation.databinding.ItemGlucoseGuideListBinding
+
+class GlucoseGuideAdapter(
+    private val glucoseGuideList: ArrayList<GlucoseGuideVO>
+) : RecyclerView.Adapter<GlucoseGuideAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding: ItemGlucoseGuideListBinding = ItemGlucoseGuideListBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GlucoseGuideAdapter.ViewHolder, position: Int) {
+        holder.bind(glucoseGuideList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return glucoseGuideList.size
+    }
+
+    inner class ViewHolder(val binding: ItemGlucoseGuideListBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+                fun bind(glucoseGuideList: GlucoseGuideVO) {
+                    binding.glucoseGuideListTitleTv.text = glucoseGuideList.guideName
+                    binding.glucoseGuideListCountTv.text = glucoseGuideList.guideCount
+                    binding.glucoseGuideListCl.background = itemView.context.getDrawable(glucoseGuideList.guideBackground)
+                }
+            }
+}
