ec2ec2ee8831079b907051517c19f326ab21fbb2
cyjadela <adelacho@naver.com>
2023-09-11 21:07:50 +0900

    Feat: 혈당 화면 조회 API - HomeFragment 구현

--- presentation/src/main/java/com/dangjang/android/presentation/home/HomeFragment.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/home/HomeFragment.kt
@@ -1,17 +1,25 @@
 package com.dangjang.android.presentation.home
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.dangjang.android.common_ui.BaseFragment
+import com.dangjang.android.domain.constants.ACCESS_TOKEN_KEY
 import com.dangjang.android.domain.constants.AUTO_LOGIN_SPF_KEY
 import com.dangjang.android.domain.constants.HEALTH_CONNECT_TOKEN_KEY
+import com.dangjang.android.domain.constants.TOKEN_SPF_KEY
 import com.dangjang.android.domain.model.GlucoseGuideVO
 import com.dangjang.android.presentation.R
 import com.dangjang.android.presentation.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 
 
 @AndroidEntryPoint
@@ -30,6 +38,19 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         super.onStart()
 
         binding.weightSeekbar.setOnTouchListener({ v, event -> true })
+
+        getAccessToken()?.let {
+                accessToken -> viewModel.getGlucose(accessToken, getTodayDate())
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.getGlucoseFlow.collectLatest {
+                if (it.todayGuides.isNullOrEmpty()) {
+                    binding.glucoseGuideNoneTv.visibility = View.VISIBLE
+                }
+                glucoseGuideAdapter.submitList(viewModel.addBackgroundToTodayGuides(it.todayGuides))
+            }
+        }
 
         binding.glucoseCl.setOnClickListener {
             Intent(activity, GlucoseActivity::class.java).apply {
@@ -69,7 +90,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     }
 
     private fun setGlucoseGuideListAdapter() {
-        glucoseGuideAdapter = GlucoseGuideAdapter(glucoseGuideList)
+        glucoseGuideAdapter = GlucoseGuideAdapter(viewModel)
 
         glucoseGuideAdapter.setMyItemClickListener(object :
             GlucoseGuideAdapter.MyItemClickListener {
@@ -82,5 +103,17 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         })
 
         binding.homeGlucoseRv.adapter = glucoseGuideAdapter
+    }
+
+    private fun getAccessToken(): String? {
+        val sharedPreferences = requireContext().getSharedPreferences(TOKEN_SPF_KEY, Context.MODE_PRIVATE)
+
+        return sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
+    }
+
+    private fun getTodayDate(): String {
+        val currentTime: Date = Calendar.getInstance().getTime()
+        val format = SimpleDateFormat("yyyy-MM-dd")
+        return format.format(currentTime)
     }
 }
