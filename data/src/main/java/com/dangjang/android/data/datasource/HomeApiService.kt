882c7908528c578e1d457bbe1a5963c8d595c9eb
cyjadela <adelacho@naver.com>
2023-09-11 21:02:10 +0900

    Feat: 혈당 화면 조회 API - ApiService 구현

--- data/src/main/java/com/dangjang/android/data/datasource/HomeApiService.kt
+++ data/src/main/java/com/dangjang/android/data/datasource/HomeApiService.kt
@@ -1,12 +1,15 @@
 package com.dangjang.android.data.datasource
 
+import com.dangjang.android.data.model.dto.GetGlucoseDto
 import com.dangjang.android.data.model.dto.HealthMetricDto
 import com.dangjang.android.domain.request.AddHealthMetricRequest
 import com.dangjang.android.data.model.response.BaseResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface HomeApiService {
 
@@ -16,4 +19,12 @@ interface HomeApiService {
         @Header("Authorization") accessToken: String,
         @Body addHealthMetricRequest: AddHealthMetricRequest
     ) : Response<BaseResponse<HealthMetricDto>>
+
+    //건강지표 조회 API
+    @GET("/api/guide/blood-sugar/{date}")
+    suspend fun getGlucose(
+        @Header("Authorization") accessToken: String,
+        @Path("date") date: String
+    ) : Response<BaseResponse<GetGlucoseDto>>
+
 }
