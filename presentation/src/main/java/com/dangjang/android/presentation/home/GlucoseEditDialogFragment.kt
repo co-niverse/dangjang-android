3cec453bf8c43da28c6999eef249b914e2539aa1
cyjadela <adelacho@naver.com>
2023-09-13 17:20:18 +0900

    Feat: 혈당 수정하기 화면 초기값 설정 및 API 구현

--- presentation/src/main/java/com/dangjang/android/presentation/home/GlucoseEditDialogFragment.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/home/GlucoseEditDialogFragment.kt
@@ -1,28 +1,39 @@
 package com.dangjang.android.presentation.home
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.dangjang.android.domain.constants.ACCESS_TOKEN_KEY
+import com.dangjang.android.domain.constants.TOKEN_SPF_KEY
 import com.dangjang.android.presentation.R
 import com.dangjang.android.presentation.databinding.FragmentGlucoseEditDialogBinding
 import com.navercorp.nid.NaverIdLoginSDK.applicationContext
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 
-class GlucoseEditDialogFragment : DialogFragment() {
+class GlucoseEditDialogFragment : DialogFragment(
+) {
+    private lateinit var binding: FragmentGlucoseEditDialogBinding
+    private val viewModel by activityViewModels<HomeViewModel>()
+    private var newType: String = ""
+    private var time: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         isCancelable = true
     }
-
-    private lateinit var binding: FragmentGlucoseEditDialogBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,7 +43,13 @@ class GlucoseEditDialogFragment : DialogFragment() {
         binding = FragmentGlucoseEditDialogBinding.inflate(inflater, container, false)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        setGlucoseTimeSpinner()
+        time = arguments?.getString("time").toString()
+        var glucose = arguments?.getString("glucose")
+
+        binding.glucoseAddEt.setText(glucose)
+        if (time != null) {
+            setGlucoseTimeSpinner(viewModel.glucoseTimeList.indexOf(time))
+        }
 
         return binding.root
     }
@@ -45,43 +62,59 @@ class GlucoseEditDialogFragment : DialogFragment() {
         }
 
         binding.glucoseAddSaveBtn.setOnClickListener {
+            viewModel.setEditGlucoseCreatedAt(getTodayDate())
+            viewModel.setEditGlucoseNewType(newType)
+            viewModel.setEditGlucoseType(time)
+            viewModel.setEditGlucoseValue(getGlucose())
+            getAccessToken()?.let { viewModel.editGlucose(it) }
             dismiss()
         }
     }
 
-    private fun setGlucoseTimeSpinner() {
+    private fun setGlucoseTimeSpinner(index: Int) {
         val glucoseSpinner: Spinner = binding.glucoseSpinner
 
-        val glucoseTimeList = arrayListOf<String>()
-        glucoseTimeList.add("공복")
-        glucoseTimeList.add("아침 식전")
-        glucoseTimeList.add("아침 식후")
-        glucoseTimeList.add("점심 식전")
-        glucoseTimeList.add("점심 식후")
-        glucoseTimeList.add("저녁 식전")
-        glucoseTimeList.add("저녁 식후")
-        glucoseTimeList.add("취침 전")
-        glucoseTimeList.add("기타")
-
-        val glucoseTimeAdapter = object : ArrayAdapter<String>(
-            applicationContext,
-            R.layout.glucose_spinner_dropdown_item, glucoseTimeList
-        ),
+        val glucoseTimeAdapter = object : ArrayAdapter<String>(applicationContext,
+            R.layout.glucose_spinner_dropdown_item, viewModel.glucoseTimeList),
             SpinnerAdapter {
-            override fun getDropDownView(
-                position: Int,
-                convertView: View?,
-                parent: ViewGroup
-            ): View {
+            override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
                 // 커스텀한 드롭다운 리스트에 표시할 뷰를 정의합니다.
                 val view = super.getDropDownView(position, convertView, parent) as TextView
                 view.setTextColor(Color.BLACK) // 드롭다운 리스트의 항목 텍스트 색상 설정
+                glucoseSpinner.setSelection(index)
                 return view
+            }
+        }
+
+
+        glucoseSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                newType = viewModel.glucoseTimeList[position]
             }
         }
 
         glucoseSpinner.adapter = glucoseTimeAdapter
 
+        glucoseSpinner.setSelection(index)
+
+    }
+
+    private fun getAccessToken(): String? {
+        val sharedPreferences = requireContext().getSharedPreferences(TOKEN_SPF_KEY, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
+    }
+
+    private fun getTodayDate(): String {
+        val currentTime: Date = Calendar.getInstance().getTime()
+        val format = SimpleDateFormat("yyyy-MM-dd")
+        return format.format(currentTime)
+    }
+
+    private fun getGlucose(): String {
+        return binding.glucoseAddEt.text.toString()
     }
 
 }
