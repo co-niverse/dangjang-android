fe331ab8bc2e5af40da5a0d02e8046ec2faec4f0
cyjadela <adelacho@naver.com>
2023-09-13 10:57:40 +0900

    Feat: 헬스커넥트 입력 API (data layer)

--- data/src/main/java/com/dangjang/android/data/datasource/SplashDataSource.kt
+++ data/src/main/java/com/dangjang/android/data/datasource/SplashDataSource.kt
@@ -1,6 +1,7 @@
 package com.dangjang.android.data.datasource
 
 import com.dangjang.android.data.model.dto.IntroDto
+import com.dangjang.android.domain.request.PostHealthConnectRequest
 import com.dangjang.android.data.model.response.BaseResponse
 import javax.inject.Inject
 
@@ -10,5 +11,12 @@ class SplashDataSource @Inject constructor(
 
     suspend fun getIntroApi(): BaseResponse<IntroDto> {
         return checkResponse(splashApiService.getIntroApi())
+    }
+
+    suspend fun postHealthConnect(
+        accessToken: String,
+        postHealthConnectRequest: PostHealthConnectRequest
+    ): BaseResponse<Nothing> {
+        return checkResponse(splashApiService.postHealthConnect(accessToken, postHealthConnectRequest))
     }
 }
--- data/src/main/java/com/dangjang/android/data/repository/SplashRepositoryImpl.kt
+++ data/src/main/java/com/dangjang/android/data/repository/SplashRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.dangjang.android.data.repository
 
 import com.dangjang.android.data.datasource.SplashDataSource
+import com.dangjang.android.domain.request.PostHealthConnectRequest
 import com.dangjang.android.domain.model.IntroVO
 import com.dangjang.android.domain.repository.SplashRepository
 import kotlinx.coroutines.flow.Flow
@@ -13,5 +14,10 @@ class SplashRepositoryImpl @Inject constructor(
     override suspend fun getIntroApi(): Flow<IntroVO> = flow {
         val response = splashDataSource.getIntroApi()
         emit(response.data.toDomain())
+    }
+
+    override suspend fun postHealthConnect(accessToken: String, postHealthConnectRequest: PostHealthConnectRequest): Flow<Nothing> = flow {
+        val response = splashDataSource.postHealthConnect(accessToken, postHealthConnectRequest)
+        emit(response.data)
     }
 }
