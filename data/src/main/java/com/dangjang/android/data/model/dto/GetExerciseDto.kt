fcd11152db20b1ad464ca6db646e127d164febb1
cyjadela <adelacho@naver.com>
2023-09-14 21:19:02 +0900

    Feat: 운동 조회하기 Dto, VO 추가

---
+++ data/src/main/java/com/dangjang/android/data/model/dto/GetExerciseDto.kt
@@ -0,0 +1,39 @@
+package com.dangjang.android.data.model.dto
+
+import com.dangjang.android.domain.constants.UNKNOWN_INT
+import com.dangjang.android.domain.constants.UNKNOWN_STRING
+import com.dangjang.android.domain.model.GetExerciseCaloriesVO
+import com.dangjang.android.domain.model.GetExerciseVO
+import com.google.gson.annotations.SerializedName
+
+data class GetExerciseDto(
+    @SerializedName("createdAt") val createdAt: String?,
+    @SerializedName("needStepByTTS") val needStepByTTS: Int?,
+    @SerializedName("needStepByLastWeek") val needStepByLastWeek: Int?,
+    @SerializedName("comparedToLastWeek") val comparedToLastWeek: String?,
+    @SerializedName("content") val content: String?,
+    @SerializedName("stepCount") val stepCount: Int?,
+    @SerializedName("exerciseCalories") val exerciseCalories: List<GetExerciseCaloriesDto>?
+) {
+    fun toDomain() = GetExerciseVO(
+        createdAt ?: UNKNOWN_STRING,
+        needStepByTTS ?: UNKNOWN_INT,
+        needStepByLastWeek ?: UNKNOWN_INT,
+        comparedToLastWeek ?: UNKNOWN_STRING,
+        content ?: UNKNOWN_STRING,
+        stepCount ?: UNKNOWN_INT,
+        exerciseCalories?.map { it.toDomain() } ?: listOf()
+    )
+}
+
+data class GetExerciseCaloriesDto(
+    @SerializedName("type") val type: String?,
+    @SerializedName("calorie") val calorie: Int?,
+    @SerializedName("exerciseTime") val exerciseTime: Int?
+) {
+    fun toDomain() = GetExerciseCaloriesVO(
+        type ?: UNKNOWN_STRING,
+        calorie ?: UNKNOWN_INT,
+        exerciseTime ?: UNKNOWN_INT
+    )
+}
---
+++ domain/src/main/java/com/dangjang/android/domain/model/GetExerciseVO.kt
@@ -0,0 +1,17 @@
+package com.dangjang.android.domain.model
+
+data class GetExerciseVO(
+    val createdAt: String = "",
+    val needStepByTTS: Int = 0,
+    val needStepByLastWeek: Int = 0,
+    val comparedToLastWeek: String = "",
+    val content: String = "",
+    val stepCount: Int = 0,
+    val exerciseCalories: List<GetExerciseCaloriesVO> = listOf()
+)
+
+data class GetExerciseCaloriesVO(
+    val type: String = "",
+    val calorie: Int = 0,
+    val exerciseTime: Int = 0
+)
