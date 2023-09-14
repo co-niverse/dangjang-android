06067e2918ffa7d14c4aff4ae7b341b62ee14561
cyjadela <adelacho@naver.com>
2023-09-14 20:51:45 +0900

    Feat: 체중 조회로 받아온 값으로 SeekBar progress 지정

--- presentation/src/main/java/com/dangjang/android/presentation/home/WeightActivity.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/home/WeightActivity.kt
@@ -8,6 +8,10 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.dangjang.android.domain.constants.ACCESS_TOKEN_KEY
+import com.dangjang.android.domain.constants.BMI_NORMAL_END
+import com.dangjang.android.domain.constants.BMI_NORMAL_START
+import com.dangjang.android.domain.constants.SEEKBAR_NORMAL_END
+import com.dangjang.android.domain.constants.SEEKBAR_NORMAL_START
 import com.dangjang.android.domain.constants.TOKEN_SPF_KEY
 import com.dangjang.android.presentation.R
 import com.dangjang.android.presentation.databinding.ActivityWeightBinding
@@ -31,6 +35,15 @@ class WeightActivity : FragmentActivity() {
         binding.lifecycleOwner = this
 
         getAccessToken()?.let { viewModel.getWeight(it) }
+
+        binding.weightSeekbar.setOnTouchListener({ v, event -> true })
+        
+        //TODO : lifecyelceScope에 없어도 되는지 테스트 해보기
+        lifecycleScope.launchWhenStarted {
+            viewModel.getWeightFlow.collectLatest {
+                binding.weightSeekbar.progress = calculateSeekbarProgress(it.bmi)
+            }
+        }
 
         binding.weightEditBtn.setOnClickListener {
             originWeight = binding.weightTv.text.toString().toInt()
@@ -74,6 +87,18 @@ class WeightActivity : FragmentActivity() {
         val sharedPreferences = getSharedPreferences(TOKEN_SPF_KEY, Context.MODE_PRIVATE)
 
         return sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
+    }
+
+    private fun calculateSeekbarProgress(bmi: Double): Int {
+        val progress: Double = if (bmi < BMI_NORMAL_START) {
+            SEEKBAR_NORMAL_START - (BMI_NORMAL_START - bmi)
+        } else if (bmi in BMI_NORMAL_START..BMI_NORMAL_END) {
+            SEEKBAR_NORMAL_START + (bmi - BMI_NORMAL_START)*((SEEKBAR_NORMAL_END - SEEKBAR_NORMAL_START)/(BMI_NORMAL_END - BMI_NORMAL_START))
+        } else {
+            SEEKBAR_NORMAL_END + (bmi - BMI_NORMAL_END)
+        }
+
+        return progress.toInt()
     }
 
 }
