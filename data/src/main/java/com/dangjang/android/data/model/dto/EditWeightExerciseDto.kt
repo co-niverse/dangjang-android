7b263fc34a6135d2ce6aa13b6d67faad63885097
cyjadela <adelacho@naver.com>
2023-09-14 16:29:48 +0900

    Chore: 체중 수정하기 Dto,VO 수정

--- data/src/main/java/com/dangjang/android/data/model/dto/EditWeightExerciseDto.kt
+++ data/src/main/java/com/dangjang/android/data/model/dto/EditWeightExerciseDto.kt
@@ -2,14 +2,29 @@ package com.dangjang.android.data.model.dto
 
 import com.dangjang.android.domain.constants.UNKNOWN_STRING
 import com.dangjang.android.domain.model.EditWeightExerciseVO
+import com.dangjang.android.domain.model.WeightExerciseGuideVO
 import com.google.gson.annotations.SerializedName
 
 data class EditWeightExerciseDto(
     @SerializedName("type") val type: String?,
     @SerializedName("title") val title: String?,
-    @SerializedName("content") val content: String?
+    @SerializedName("content") val content: String?,
+    @SerializedName("guide") val guide: WeightExerciseGuideDto?
 ) {
     fun toDomain() = EditWeightExerciseVO(
+        type ?: UNKNOWN_STRING,
+        title ?: UNKNOWN_STRING,
+        content ?: UNKNOWN_STRING,
+        guide?.toDomain() ?: WeightExerciseGuideVO()
+    )
+}
+
+data class WeightExerciseGuideDto(
+    @SerializedName("type") val type: String?,
+    @SerializedName("title") val title: String?,
+    @SerializedName("content") val content: String?
+) {
+    fun toDomain() = WeightExerciseGuideVO(
         type ?: UNKNOWN_STRING,
         title ?: UNKNOWN_STRING,
         content ?: UNKNOWN_STRING
--- domain/src/main/java/com/dangjang/android/domain/model/EditWeightExerciseVO.kt
+++ domain/src/main/java/com/dangjang/android/domain/model/EditWeightExerciseVO.kt
@@ -2,6 +2,13 @@ package com.dangjang.android.domain.model
 
 data class EditWeightExerciseVO(
     val type: String = "",
+    val createdAt: String = "",
+    val unit: String = "",
+    val guide: WeightExerciseGuideVO = WeightExerciseGuideVO()
+)
+
+data class WeightExerciseGuideVO(
+    val type: String = "",
     val title: String = "",
     val content: String = ""
 )
