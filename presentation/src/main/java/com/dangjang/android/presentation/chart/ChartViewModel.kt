cbc44a146592b5c35ecc7376465a375f117c11bf
cyjadela <adelacho@naver.com>
2023-09-18 11:23:44 +0900

    Feat: 건강차트 조회 API (viewmodel) 구현

--- presentation/src/main/java/com/dangjang/android/presentation/chart/ChartViewModel.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/chart/ChartViewModel.kt
@@ -1,10 +1,48 @@
 package com.dangjang.android.presentation.chart
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.dangjang.android.domain.model.GetChartVO
+import com.dangjang.android.domain.usecase.ChartUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ChartViewModel @Inject constructor(
-) : ViewModel() {
+    private val getChartUseCase: ChartUseCase,
+    application: Application
+) : AndroidViewModel(application) {
+
+    private val _getChartFlow = MutableStateFlow(GetChartVO())
+    val getChartFlow = _getChartFlow.asStateFlow()
+
+    fun getChart(accessToken: String, startDate: String, endDate: String) {
+        viewModelScope.launch {
+            getChartUseCase.getChart("Bearer $accessToken", startDate, endDate)
+                .onEach {
+                    _getChartFlow.emit(it)
+                }
+                .handleErrors()
+                .collect()
+        }
+    }
+
+    private fun <T> Flow<T>.handleErrors(): Flow<T> =
+        catch { e ->
+            Log.e("error",e.message.toString())
+            Toast.makeText(
+                getApplication<Application>().applicationContext, e.message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 }
