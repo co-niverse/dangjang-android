d36ae4ba45275e6b5a282aee75cfe63e820bbbec
cyjadela <adelacho@naver.com>
2023-09-26 19:22:48 +0900

    Feat: 포인트 - 신청 완료 화면 UI 구현

---
+++ presentation/src/main/java/com/dangjang/android/presentation/mypage/PointDoneFragment.kt
@@ -0,0 +1,24 @@
+package com.dangjang.android.presentation.mypage
+
+import androidx.fragment.app.activityViewModels
+import com.dangjang.android.common_ui.BaseFragment
+import com.dangjang.android.presentation.R
+import com.dangjang.android.presentation.databinding.FragmentPointDoneBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PointDoneFragment : BaseFragment<FragmentPointDoneBinding>(R.layout.fragment_point_done){
+
+    private val viewModel : MypageViewModel by activityViewModels()
+
+    override fun initView() {
+        bind {
+            vm = viewModel
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+}
