1b2c3b02e3296ce3684b9bb2e059ad4b12d98c34
cyjadela <adelacho@naver.com>
2023-10-06 12:22:06 +0900

    Feat: 포인트 전화번호 입력 구현

--- presentation/src/main/java/com/dangjang/android/presentation/mypage/PointPhoneFragment.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/mypage/PointPhoneFragment.kt
@@ -1,5 +1,6 @@
 package com.dangjang.android.presentation.mypage
 
+import android.os.Bundle
 import androidx.fragment.app.activityViewModels
 import com.dangjang.android.common_ui.BaseFragment
 import com.dangjang.android.presentation.R
@@ -20,12 +21,22 @@ class PointPhoneFragment : BaseFragment<FragmentPointPhoneBinding>(R.layout.frag
     override fun onStart() {
         super.onStart()
 
+        val type = arguments?.getString("type").toString()
+        val price = arguments?.getString("price").toString()
+
+        binding.giftTitleTv.text = type
+        binding.giftPointTv.text = price + " 포인트"
+
         binding.backIv.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
 
         binding.nextBtn.setOnClickListener {
             val pointCheckFragment = PointCheckFragment()
+            var bundle = Bundle()
+            bundle.putString("type", type)
+            bundle.putString("price", price)
+            bundle.putString("phone", binding.phoneEt.text.toString())
             parentFragmentManager.beginTransaction().replace(R.id.point_cl, pointCheckFragment).addToBackStack(null).commit()
         }
     }
