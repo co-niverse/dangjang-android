0bd8f9da06a7541d1ccbbdea17376cca9cdf50d1
cyjadela <adelacho@naver.com>
2023-10-08 21:14:54 +0900

    Feat: Application에서 logging init 처리

--- app/src/main/java/com/dangjang/android/DangjangApplication.kt
+++ app/src/main/java/com/dangjang/android/DangjangApplication.kt
@@ -1,8 +1,12 @@
 package com.dangjang.android
 
 import android.app.Application
+import android.content.Context
+import com.dangjang.android.domain.constants.ACCESS_TOKEN_KEY
+import com.dangjang.android.domain.constants.TOKEN_SPF_KEY
 import com.kakao.sdk.common.KakaoSdk
 import com.navercorp.nid.NaverIdLoginSDK
+import com.dangjang.android.swm_logging.SWMLogging
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -13,7 +17,25 @@ class DangjangApplication : Application() {
         KakaoSdk.init(this, BuildConfig.KAKAO_APP_KEY)
         NaverIdLoginSDK.initialize(this, BuildConfig.NAVER_CLIENT_ID, BuildConfig.NAVER_CLIENT_SECRET, "dangjang")
 
+        SWMLogging.init(
+            //appVersion = BuildConfig.VERSION_NAME,
+            //osNameAndVersion = "$ANDROID ${android.os.Build.VERSION.SDK_INT}",
+            baseUrl = com.dangjang.android.data.BuildConfig.BASE_URL,
+            serverPath = "api/log",
+            token = getAccessToken() ?: ""
+        )
+
         //TODO : 앱 꺼짐 방지 에러 핸들링 -> 홈 화면 이동
         Thread.setDefaultUncaughtExceptionHandler { t, e ->  }
+    }
+
+    companion object {
+        private const val ANDROID = "Android"
+    }
+
+    private fun getAccessToken(): String? {
+        val sharedPreferences = applicationContext.getSharedPreferences(TOKEN_SPF_KEY, Context.MODE_PRIVATE)
+
+        return sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
     }
 }
