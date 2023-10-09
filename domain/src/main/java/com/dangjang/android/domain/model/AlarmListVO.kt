65652b33b96404067d96f225d471ab7fd08b2f1a
cyjadela <adelacho@naver.com>
2023-10-09 18:03:50 +0900

    Feat: 알람 목록 Recyclerview 구현

---
+++ domain/src/main/java/com/dangjang/android/domain/model/AlarmListVO.kt
@@ -0,0 +1,9 @@
+package com.dangjang.android.domain.model
+
+data class AlarmListVO(
+    val notificationId: Int = 0,
+    val title: String = "",
+    val content: String = "",
+    val type: String = "",
+    val createdAt: String = ""
+)
--- presentation/src/main/java/com/dangjang/android/presentation/home/AlarmActivity.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/home/AlarmActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
 import com.dangjang.android.presentation.R
 import com.dangjang.android.presentation.databinding.ActivityAlarmBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class AlarmActivity : FragmentActivity() {
     private lateinit var binding: ActivityAlarmBinding
     private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var alarmListAdapter: AlarmListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +22,22 @@ class AlarmActivity : FragmentActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_alarm)
         binding.lifecycleOwner = this
 
+        setAlarmListAdapter()
+
+        lifecycleScope.launchWhenStarted {
+//            viewModel.getAlarmFlow.collectLatest {
+//                alarmListAdapter.submitList()
+//            }
+        }
+
         binding.backIv.setOnClickListener {
             finish()
         }
+    }
+
+    private fun setAlarmListAdapter() {
+        alarmListAdapter = AlarmListAdapter(viewModel)
+        binding.alarmRv.adapter = alarmListAdapter
     }
 
 }
