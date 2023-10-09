5b79b45e3499eb99f6e4c90789af7e357a027aeb
cyjadela <adelacho@naver.com>
2023-10-09 11:02:37 +0900

    Feat: 회원탈퇴 API 연동 (presentation layer)

--- presentation/src/main/java/com/dangjang/android/presentation/mypage/MypageViewModel.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/mypage/MypageViewModel.kt
@@ -48,6 +48,9 @@ class MypageViewModel @Inject constructor(
     private val _selectedGiftPhone = MutableStateFlow(String())
     val selectedGiftPhone = _selectedGiftPhone.asStateFlow()
 
+    private val _signoutFlow = MutableStateFlow(false)
+    val signoutFlow = _signoutFlow.asStateFlow()
+
     fun getMypage(accessToken: String) {
         viewModelScope.launch {
             getMypageUseCase.getMypage("Bearer $accessToken")
@@ -99,6 +102,17 @@ class MypageViewModel @Inject constructor(
             getMypageUseCase.postPoint("Bearer $accessToken", postPointRequest.value)
                 .onEach {
                     _postPointFlow.emit(it)
+                }
+                .handleErrors()
+                .collect()
+        }
+    }
+
+    fun signout(accessToken: String) {
+        viewModelScope.launch {
+            getMypageUseCase.signout("Bearer $accessToken")
+                .onEach {
+                    _signoutFlow.emit(it)
                 }
                 .handleErrors()
                 .collect()
--- presentation/src/main/java/com/dangjang/android/presentation/mypage/SignoutDialogFragment.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/mypage/SignoutDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.dangjang.android.presentation.mypage
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -7,7 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.dangjang.android.domain.constants.ACCESS_TOKEN_KEY
+import com.dangjang.android.domain.constants.TOKEN_SPF_KEY
 import com.dangjang.android.presentation.databinding.FragmentSignoutDialogBinding
+import kotlinx.coroutines.flow.collectLatest
 
 class SignoutDialogFragment: DialogFragment() {
 
@@ -17,6 +23,7 @@ class SignoutDialogFragment: DialogFragment() {
     }
 
     private lateinit var binding: FragmentSignoutDialogBinding
+    private val viewModel by activityViewModels<MypageViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,11 +41,24 @@ class SignoutDialogFragment: DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.signoutBtn.setOnClickListener {
-            //TODO : 회원탈퇴 API 연동
+            getAccessToken()?.let { viewModel.signout(it) }
         }
 
         binding.dismissBtn.setOnClickListener {
             dismiss()
         }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.signoutFlow.collectLatest {
+                if (it) {
+                    dismiss()
+                }
+            }
+        }
+    }
+
+    private fun getAccessToken(): String? {
+        val sharedPreferences = requireContext().getSharedPreferences(TOKEN_SPF_KEY, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
     }
 }
