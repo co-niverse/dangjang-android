a6c744d7c3bd7385fabc0fcc57142d645da7d394
cyjadela <adelacho@naver.com>
2023-10-09 14:52:50 +0900

    Feat: 토큰 재발급 요청 에러 처리 구현

--- presentation/src/main/java/com/dangjang/android/presentation/mypage/MypageViewModel.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/mypage/MypageViewModel.kt
@@ -1,15 +1,21 @@
 package com.dangjang.android.presentation.mypage
 
 import android.app.Application
+import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.dangjang.android.domain.constants.ACCESS_TOKEN_KEY
+import com.dangjang.android.domain.constants.TOKEN_SPF_KEY
 import com.dangjang.android.domain.model.GetMypageVO
 import com.dangjang.android.domain.model.GetPointVO
 import com.dangjang.android.domain.model.PostPointVO
 import com.dangjang.android.domain.request.PostPointRequest
 import com.dangjang.android.domain.usecase.MypageUseCase
+import com.dangjang.android.domain.usecase.TokenUseCase
+import com.dangjang.android.presentation.login.LoginActivity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -23,6 +29,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MypageViewModel @Inject constructor(
+    private val getTokenUseCase: TokenUseCase,
     private val getMypageUseCase: MypageUseCase,
     application: Application
 ) : AndroidViewModel(application) {
@@ -53,6 +60,9 @@ class MypageViewModel @Inject constructor(
 
     private val _logoutFlow = MutableStateFlow(false)
     val logoutFlow = _logoutFlow.asStateFlow()
+
+    private val _reissueTokenFlow = MutableStateFlow(false)
+    val reissueTokenFlow = _reissueTokenFlow.asStateFlow()
 
     fun getMypage(accessToken: String) {
         viewModelScope.launch {
@@ -133,12 +143,51 @@ class MypageViewModel @Inject constructor(
         }
     }
 
+
+    private fun getAccessToken(): String? {
+        val sharedPreferences = getApplication<Application>().applicationContext.getSharedPreferences(
+            TOKEN_SPF_KEY, Context.MODE_PRIVATE)
+
+        return sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
+    }
+
     private fun <T> Flow<T>.handleErrors(): Flow<T> =
         catch { e ->
             Log.e("error",e.message.toString())
-            Toast.makeText(
-                getApplication<Application>().applicationContext, e.message,
-                Toast.LENGTH_SHORT
-            ).show()
+            if (e.message.toString() == "만료된 토큰입니다.") {
+                getTokenUseCase.reissueToken(getAccessToken() ?: "")
+                    .onEach {
+                        _reissueTokenFlow.emit(it)
+                    }
+                    .handleReissueTokenErrors()
+                    .collect()
+                Toast.makeText(
+                    getApplication<Application>().applicationContext, "로그인이 만료되었습니다. 다시 한번 시도해주세요.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+//            Toast.makeText(
+//                getApplication<Application>().applicationContext, e.message,
+//                Toast.LENGTH_SHORT
+//            ).show()
+        }
+
+    private fun <T> Flow<T>.handleReissueTokenErrors(): Flow<T> =
+        catch { e ->
+            Log.e("error",e.message.toString())
+            // refreshToken까지 만료된 경우 -> 로그인 화면으로 이동
+            if (e.message.toString() == "만료된 토큰입니다.") {
+                Intent(getApplication<Application>().applicationContext, LoginActivity::class.java).apply {
+                    getApplication<Application>().applicationContext.startActivity(this)
+                }
+                Toast.makeText(
+                    getApplication<Application>().applicationContext, "로그인이 필요합니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+//            Toast.makeText(
+//                getApplication<Application>().applicationContext, e.message,
+//                Toast.LENGTH_SHORT
+//            ).show()
         }
 }
