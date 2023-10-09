791f99a0a6cd94d1575e5578070d6212b96e59f2
cyjadela <adelacho@naver.com>
2023-10-09 18:29:45 +0900

    Feat: 알람 목록 조회 API 구현 (domain layer)

--- domain/src/main/java/com/dangjang/android/domain/model/GetNotificationVO.kt
+++ domain/src/main/java/com/dangjang/android/domain/model/GetNotificationVO.kt
@@ -1,5 +1,9 @@
 package com.dangjang.android.domain.model
 
+data class GetNotificationVO(
+    val notificationList: List<AlarmListVO> = listOf()
+)
+
 data class AlarmListVO(
     val notificationId: Int = 0,
     val title: String = "",
