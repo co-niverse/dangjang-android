04992191110936235bbbe36608274c8bd865f797
cyjadela <adelacho@naver.com>
2023-10-15 15:14:30 +0900

    Feat: 로그아웃, 회원탈퇴 실패 처리

--- presentation/src/main/java/com/dangjang/android/presentation/mypage/LogoutDialogFragment.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/mypage/LogoutDialogFragment.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -45,26 +46,30 @@ class LogoutDialogFragment : DialogFragment() {
 
         binding.logoutBtn.setOnClickListener {
             viewModel.logout(getAccessToken() ?: "", getFCMToken() ?: "")
+            lifecycleScope.launchWhenStarted {
+                viewModel.logoutFlow.collectLatest {
+                    if (it) {
+                        viewModel.removeAutoLoginProviderSpf()
+                        viewModel.removeAccessTokenSpf()
+                        viewModel.removeFcmTokenSpf()
+
+                        val intent = Intent(requireContext(), LoginActivity::class.java)
+                        startActivity(intent)
+
+                        activity?.finish()
+                    } else {
+                        Toast.makeText(requireContext(), "다시 한번 시도해주세요.", Toast.LENGTH_SHORT)
+                            .show()
+                        dismiss()
+                    }
+                }
+            }
         }
 
         binding.dismissBtn.setOnClickListener {
             dismiss()
         }
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.logoutFlow.collectLatest {
-                if (it) {
-                    viewModel.removeAutoLoginProviderSpf()
-                    viewModel.removeAccessTokenSpf()
-                    viewModel.removeFcmTokenSpf()
-
-                    val intent = Intent(requireContext(), LoginActivity::class.java)
-                    startActivity(intent)
-
-                    activity?.finish()
-                }
-            }
-        }
     }
 
     private fun getFCMToken(): String? {
--- presentation/src/main/java/com/dangjang/android/presentation/mypage/SignoutDialogFragment.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/mypage/SignoutDialogFragment.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -44,25 +45,28 @@ class SignoutDialogFragment: DialogFragment() {
 
         binding.signoutBtn.setOnClickListener {
             getAccessToken()?.let { viewModel.signout(it) }
+            lifecycleScope.launchWhenStarted {
+                viewModel.signoutFlow.collectLatest {
+                    if (it) {
+                        viewModel.removeAutoLoginProviderSpf()
+                        viewModel.removeAccessTokenSpf()
+                        viewModel.removeFcmTokenSpf()
+
+                        val intent = Intent(requireContext(), LoginActivity::class.java)
+                        startActivity(intent)
+
+                        activity?.finish()
+                    } else {
+                        Toast.makeText(requireContext(), "다시 한번 시도해주세요.", Toast.LENGTH_SHORT)
+                            .show()
+                        dismiss()
+                    }
+                }
+            }
         }
 
         binding.dismissBtn.setOnClickListener {
             dismiss()
-        }
-
-        lifecycleScope.launchWhenStarted {
-            viewModel.signoutFlow.collectLatest {
-                if (it) {
-                    viewModel.removeAutoLoginProviderSpf()
-                    viewModel.removeAccessTokenSpf()
-                    viewModel.removeFcmTokenSpf()
-
-                    val intent = Intent(requireContext(), LoginActivity::class.java)
-                    startActivity(intent)
-
-                    activity?.finish()
-                }
-            }
         }
     }
 
