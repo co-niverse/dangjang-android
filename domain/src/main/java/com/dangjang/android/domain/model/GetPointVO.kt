c4e8f5b7e54e8138feb0b7adf64996859ff24c0a
cyjadela <adelacho@naver.com>
2023-10-23 22:09:18 +0900

    Feat: 포인트 적립 방법 서버에서 내려온 값으로 구현

--- domain/src/main/java/com/dangjang/android/domain/model/GetPointVO.kt
+++ domain/src/main/java/com/dangjang/android/domain/model/GetPointVO.kt
@@ -2,7 +2,8 @@ package com.dangjang.android.domain.model
 
 data class GetPointVO(
     val balancedPoint: Int = 0,
-    val products: List<ProductVO> = listOf()
+    val products: List<ProductVO> = listOf(),
+    val descriptionListToEarnPoint: List<String> = listOf()
 )
 
 data class ProductVO(
