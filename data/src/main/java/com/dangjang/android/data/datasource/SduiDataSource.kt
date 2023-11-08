497191d2c9229d5453cef47dc0a291c5c1c16cb5
cyjadela <adelacho@naver.com>
2023-11-08 14:56:45 +0900

    Feat: 회원가입 팝업창 Sever driven UI (data layer) 구현

---
+++ data/src/main/java/com/dangjang/android/data/datasource/SduiDataSource.kt
@@ -0,0 +1,16 @@
+package com.dangjang.android.data.datasource
+
+import com.dangjang.android.data.model.response.BaseResponse
+import com.dangjang.android.data.sdui.SduiSignupDto
+import com.dangjang.android.domain.sdui.SduiCommonVO
+import javax.inject.Inject
+
+class SduiDataSource @Inject constructor(
+    private val sduiApiService: SduiApiService
+) : BaseNetworkDataSource() {
+
+    suspend fun getSduiSignup() : BaseResponse<SduiSignupDto> {
+        return checkResponse(sduiApiService.getSduiSignup())
+    }
+
+}
