0f273e81bbb60e298a03018a9f6666fb6a58ba62
cyjadela <adelacho@naver.com>
2023-11-08 19:27:51 +0900

    Fix: 포인트 기프티콘 금액 미달일 때 선택되는 문제 해결

--- presentation/src/main/java/com/dangjang/android/presentation/intro/SplashActivity.kt
+++ presentation/src/main/java/com/dangjang/android/presentation/intro/SplashActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.dangjang.android.domain.constants.KAKAO
 import com.dangjang.android.domain.constants.NAVER
 import com.dangjang.android.presentation.login.LoginActivity
-import com.dangjang.android.presentation.signup.SignupActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,10 +21,10 @@ class SplashActivity : AppCompatActivity() {
     private fun goToMainOrLoginActivity() {
         if (viewModel.getAutoLoginProviderSpf() == KAKAO || viewModel.getAutoLoginProviderSpf() == NAVER) {
             //TODO : go to health connect activity
-            startActivity(Intent(this, SignupActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         } else {
-            startActivity(Intent(this, SignupActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
     }
